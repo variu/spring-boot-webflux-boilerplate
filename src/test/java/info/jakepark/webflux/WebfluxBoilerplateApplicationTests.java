4c0d18b449e9075df21193e54f08a11b3fe81013
jake-park <variu@naver.com>
2022-03-12 19:05:30 +0900

    gradle init

---
+++ src/test/java/info/jakepark/webflux/WebfluxBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package info.jakepark.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
